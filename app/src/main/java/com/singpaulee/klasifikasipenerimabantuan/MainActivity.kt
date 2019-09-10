fd10450b155312198752921508bfd5f4b4938fab
hanifabdullah <hanifabdullah7@gmail.com>
2019-09-10 22:32:22 +0700

    Change primary, dark-primary and accent color

--- app/src/main/java/com/singpaulee/klasifikasipenerimabantuan/MainActivity.kt
+++ app/src/main/java/com/singpaulee/klasifikasipenerimabantuan/MainActivity.kt
@@ -7,18 +7,20 @@ import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.intentFor
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    override fun onClick(v: View?) {
-        when(v){
-            main_cv_classification -> {
-                startActivity(intentFor<QuestionnaireActivity>())
-            }
-        }
-    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         main_cv_classification.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v){
+            main_cv_classification -> {
+                startActivity(intentFor<QuestionnaireActivity>())
+            }
+        }
     }
 }
