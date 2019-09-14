dd9611f2831f52db63f99fcd0a79d258aabf6509
hanif <hanifabdullah7@gmail.com>
2019-09-14 16:11:15 +0700

    Update Result Classification

--- app/src/main/java/com/singpaulee/klasifikasipenerimabantuan/ResultActivity.kt
+++ app/src/main/java/com/singpaulee/klasifikasipenerimabantuan/ResultActivity.kt
@@ -1,6 +1,7 @@
 package com.singpaulee.klasifikasipenerimabantuan
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -14,13 +15,17 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_questionnaire.*
 import kotlinx.android.synthetic.main.activity_result.*
+import kotlinx.android.synthetic.main.view_loading.*
+import org.jetbrains.anko.backgroundColor
 import org.jetbrains.anko.toast
 
-class ResultActivity : AppCompatActivity(), LoadingInterface {
+class ResultActivity : AppCompatActivity(), LoadingInterface, View.OnClickListener {
 
     val TAG = "RESULT"
+    var STAGE: Int = 0
 
     var qAnswerModel: QuessionnaireObjectModel? = null
+    var resultClassification: ClassificationDataModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,10 +35,16 @@ class ResultActivity : AppCompatActivity(), LoadingInterface {
         Log.d(TAG, "Questionnaire Answer $qAnswerModel")
 
         getListDataTraining()
+
+        ra_btn_add_to_db.setOnClickListener(this)
+        ra_btn_back_to_main_menu.setOnClickListener(this)
+        vl_btn_reload.setOnClickListener(this)
+
     }
 
     @SuppressLint("CheckResult")
     fun getListDataTraining() {
+        STAGE = 1
         showLoading()
         val dataTraining = AppConfig.retrofitConfig(this)
             .create<DataInterface>(DataInterface::class.java)
@@ -50,33 +61,70 @@ class ResultActivity : AppCompatActivity(), LoadingInterface {
                     toast("failed")
                 }
             }, {
-                showLoading()
+                hideLoading()
+                showErrorMessage(it.localizedMessage)
                 toast(it.localizedMessage)
+            })
+    }
+
+    @SuppressLint("CheckResult")
+    fun postDataClassification(){
+        STAGE=2
+        showLoading()
+        val postData = AppConfig.retrofitConfig(this)
+            .create(DataInterface::class.java)
+            .postClassificationData(
+                intent?.getStringExtra("name").toString(),
+                resultClassification?.education?.weight,
+                resultClassification?.family?.weight,
+                resultClassification?.job?.weight,
+                resultClassification?.income?.weight,
+                resultClassification?.house?.weight,
+                resultClassification?.status
+            )
+
+        postData.subscribeOn(Schedulers.newThread())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                hideLoading()
+                if (it?.success as Boolean){
+                    ra_btn_add_to_db.text = "Berhasil ditambahkan"
+                    ra_btn_add_to_db.backgroundColor = Color.GRAY
+                    ra_btn_add_to_db.isEnabled = false
+                }else{
+                    showErrorMessage(it.message.toString())
+                }
+            }, {
+                hideLoading()
+                showErrorMessage(it.localizedMessage)
             })
     }
 
     private fun executeKNN(data: ArrayList<ClassificationDataModel>?) {
         val knn = KNearestNeighbour()
 
-        val resultClassification = knn.doClassification(qAnswerModel, data, 3)
+        resultClassification = knn.doClassification(qAnswerModel, data, 3)
 
-        Log.d("RESULT", resultClassification.status)
+        Log.d("RESULT", resultClassification?.status)
         setContent(resultClassification)
     }
 
-    private fun setContent(resultClassification: ClassificationDataModel) {
+    private fun setContent(resultClassification: ClassificationDataModel?) {
         ra_tv_name.text = intent.getStringExtra("name")
-        ra_tv_education.text = resultClassification.education?.variable.toString()
-        ra_tv_family.text = resultClassification.family?.variable.toString()
-        ra_tv_job.text = resultClassification.job?.variable.toString()
-        ra_tv_house.text = resultClassification.house?.variable.toString()
-        ra_tv_income.text = resultClassification.income?.variable.toString()
-        ra_tv_status.text = resultClassification.status.toString()
+        ra_tv_education.text = resultClassification?.education?.variable.toString()
+        ra_tv_family.text = resultClassification?.family?.variable.toString()
+        ra_tv_job.text = resultClassification?.job?.variable.toString()
+        ra_tv_house.text = resultClassification?.house?.variable.toString()
+        ra_tv_income.text = resultClassification?.income?.variable.toString()
+        ra_tv_status.text = resultClassification?.status.toString()
     }
 
     override fun showLoading() {
         ra_viewgroup_content.visibility = View.GONE
         ra_viewgroup_loading.visibility = View.VISIBLE
+
+        vl_textview.text = "Loading..."
+        vl_btn_reload.visibility = View.GONE
     }
 
     override fun hideLoading() {
@@ -85,6 +133,22 @@ class ResultActivity : AppCompatActivity(), LoadingInterface {
     }
 
     override fun showErrorMessage(message: String) {
+        ra_viewgroup_content.visibility = View.GONE
+        ra_viewgroup_loading.visibility = View.VISIBLE
+        vl_textview.text = message
+        vl_btn_reload.visibility = View.VISIBLE
+    }
 
+    override fun onClick(p0: View?) {
+        when(p0){
+            ra_btn_add_to_db -> postDataClassification()
+            ra_btn_back_to_main_menu -> onBackPressed()
+            vl_btn_reload -> {
+                when(STAGE){
+                    1 -> getListDataTraining()
+                    2 -> postDataClassification()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/singpaulee/klasifikasipenerimabantuan/connection/DataInterface.kt
+++ app/src/main/java/com/singpaulee/klasifikasipenerimabantuan/connection/DataInterface.kt
@@ -3,8 +3,7 @@ package com.singpaulee.klasifikasipenerimabantuan.connection
 import com.singpaulee.klasifikasipenerimabantuan.model.ResponseDataTraining
 import com.singpaulee.klasifikasipenerimabantuan.model.ResponseQuestionnaire
 import io.reactivex.Observable
-import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface DataInterface {
 
@@ -17,4 +16,17 @@ interface DataInterface {
     fun getListData(
         @Query("action") action: String?
     ): Observable<ResponseDataTraining>
+
+    @FormUrlEncoded
+    @POST("exec")
+    fun postClassificationData(
+        @Field("name") name: String,
+        @Field("education") education: Int?,
+        @Field("family") family: Int?,
+        @Field("job") job: Int?,
+        @Field("income") income: Int?,
+        @Field("house") house: Int?,
+        @Field("status") status: String?
+    ): Observable<ResponseDataTraining>
+
 }
